25d6f245e59d1eb266c04762b28d1443cd15fedb
Joey Meyer <jmeyer41@gmail.com>
2016-11-03 12:27:18 -0400

    create helper functions for /var paths

--- raspberryturk/__init__.py
+++ raspberryturk/__init__.py
@@ -2,3 +2,23 @@ import os
 from logging.config import fileConfig
 base_path = os.path.dirname(os.path.realpath(__file__))
 fileConfig(os.path.join(base_path, 'logging.ini'))
+
+app_directory = 'raspberryturk'
+
+def _root():
+    return os.path.abspath(os.sep)
+
+def _var_subdir(subdir, *paths):
+    return os.path.join(_root(), 'var', subdir, app_directory, *paths)
+
+def cache_path(*paths):
+    return _var_subdir('cache', *paths)
+
+def lib_path(*paths):
+    return _var_subdir('lib', *paths)
+
+def games_path(*paths):
+    return _var_subdir('games', *paths)
+
+def opt_path(*paths):
+    return _var_subdir('opt', *paths)
