1afb380bff63e70014e9c9f6c39d6c9050b92377
Joey Meyer <jmeyer41@gmail.com>
2016-11-05 14:00:48 -0400

    add support for temporary chess games

--- raspberryturk/embedded/chess/game.py
+++ raspberryturk/embedded/chess/game.py
@@ -1,20 +1,39 @@
+import os
+from time import time
 from raspberryturk import games_path
 from chess.pgn import read_game, Game, FileExporter
 
-def _pgn_path():
-    return games_path('game.pgn')
+TEMPORARY_GAME_PATH = os.path.extsep.join(['tmp', 'pgn'])
+CURRENT_GAME_PATH = games_path(os.path.extsep.join(['game', 'pgn']))
 
 def _game():
-    pgn = open(_pgn_path())
-    return read_game(pgn)
+    with open(CURRENT_GAME_PATH) as pgn:
+        return read_game(pgn)
 
-def _save_game(game):
-    pgn = open(_pgn_path(), 'w')
+def _save_game(game, path=CURRENT_GAME_PATH):
+    pgn = open(path, 'w')
     exporter = FileExporter(pgn)
     game.accept(exporter)
+    pgn.close()
 
-def start_new_game():
-    _save_game(Game())
+def is_temporary():
+    base = os.path.basename(os.path.realpath(CURRENT_GAME_PATH))
+    return base == TEMPORARY_GAME_PATH
+
+def start_new_game(temporary=False):
+    base = 'tmp' if temporary else str(int(time()))
+    fn = os.path.extsep.join([base, 'pgn'])
+    path = games_path(fn)
+    _save_game(Game(), path)
+    enter_game(fn)
+
+def enter_game(fn):
+    try:
+        os.unlink(CURRENT_GAME_PATH)
+    except OSError as e:
+        if e.errno != 2:
+            raise e
+    os.symlink(fn, CURRENT_GAME_PATH)
 
 def get_board():
     g = _game()
